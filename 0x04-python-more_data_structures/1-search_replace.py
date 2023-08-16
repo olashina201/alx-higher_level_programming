af84092045ae9e52929f2c3c18eedd90f687760c
olashina201 <olashina201@gmail.com>
2023-08-17 00:25:20 +0100

    update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def search_replace(my_list, search, replace);
+    """
+    A function that replacrs all occurrences
+    of an element by another in a new list
+    """
+    copy = my_list.copy()
+
+    for i in range(len(my_list)):
+        if my_list[i] == search
+            copy[i] = replace
+
+    return (copy)
