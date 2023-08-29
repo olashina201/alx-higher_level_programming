7d83998c6a51fb5dbae5c76d10001090fa1b72c0
olashina201 <olashina201@gmail.com>
2023-08-30 00:01:08 +0100

    udate

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+"""Define a class named Square."""
+
+
 class Square:
+    """Represents a Square."""
+
     def __init__(self, size):
+        """Initialize a new square.
+        
+        Args:
+            size (int): size of the new square.
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
+"""Define a class named Square."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """Initializes a new square.
+        
+        Args:
+            size (int): size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
